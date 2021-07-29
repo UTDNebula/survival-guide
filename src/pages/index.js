f5dbf8438603dca8d767389d8ed3fcb3d42e0982
vishvakbandi <vishvakbandi@gmail.com>
2021-07-28 22:54:43 -0500

    refactor: Seperate landing page into component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+import Landing from '../components/Landing';
 
 // TODO
 // - Seperate the landing page into a component
@@ -12,58 +13,35 @@ import Seo from "../components/seo"
 // - config the config file more
 
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+  const siteTitle = data.site.siteMetadata?.title || `Title`;
+  const posts = data.allMarkdownRemark.nodes;
 
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
+        <Landing />
         <Seo title="All posts" />
         <Bio />
         <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
+          No blog posts found. Add markdown posts to "content/blog" (or the directory you specified
+          for the "gatsby-source-filesystem" plugin in gatsby-config.js).
         </p>
       </Layout>
-    )
+    );
   }
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="Home" />
-      <div className="lg:flex h-full">
-        <section className="flex-1 flex flex-col justify-center px-16 py-40 color-light text-dark">
-          <div className="my-4">
-            <div className="text-headline3 font-bold">UTD Survival Guide</div>
-            <div className="text-headline4">
-              Your ticket to the next few years.
-            </div>
-            <div className="py-4 text-headline5 font-bold text-blue-400 underline">
-              <a href="https://github.com/acmutd/nebula-guide">
-                &gt; Coming soon.
-              </a>
-            </div>
-          </div>
-        </section>
-        <section className="flex-1 px-16 bg-primary-light">
-          {/* TODO: Nebula graphic and other announcements */}
-          {/* TODO: Actually, maybe a testimonial like web.mit.edu */}
-        </section>
-      </div>
+      <Landing />
       <Seo title="All posts" />
       <Bio />
       <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+        {posts.map((post) => {
+          const title = post.frontmatter.title || post.fields.slug;
 
           return (
             <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
+              <article className="post-list-item" itemScope itemType="http://schema.org/Article">
                 <header>
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
@@ -82,14 +60,14 @@ const BlogIndex = ({ data, location }) => {
                 </section>
               </article>
             </li>
-          )
+          );
         })}
       </ol>
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query {
@@ -112,4 +90,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
