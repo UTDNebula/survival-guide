e65e19461132ffd38519c036cf900880451bbe0e
Joseph C <84756918+JosephChang1@users.noreply.github.com>
2022-10-24 22:12:11 -0500

    Separate Components from Sidebar

---
+++ components/Section.js
@@ -0,0 +1,11 @@
+
+export default function Section(props){
+    return (
+        <ul>
+            <li className='section'>{props.sectionName}</li>
+            <li><Article url="https://www.utdallas.edu/" articleName="UT Dallas"/></li>
+            <li><Article url="https://github.com/UTDNebula" articleName="UTD Nebula"/></li>
+            <li><Article url="https://github.com/UTDNebula/survival-guide" articleName="Survival Guide"/></li>  
+        </ul>
+    )
+};
