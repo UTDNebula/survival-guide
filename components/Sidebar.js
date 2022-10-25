1861ccd49a414b5c84199c22b03f26c352fd679b
Joseph C <84756918+JosephChang1@users.noreply.github.com>
2022-10-24 22:13:43 -0500

    Separate components from Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import './Sidebar.css';
-
+import Section from './Section.js';
 
 export default function Sidebar(props){
     return(
         <div className='sidebar'>
             <Section sectionName='Introduction'/>
         </div>
-    )
-};
-
-function Section(props){
-    return (
-        <ul>
-            <li className='section'>{props.sectionName}</li>
-            <li><Article url="https://www.utdallas.edu/" articleName="UT Dallas"/></li>
-            <li><Article url="https://github.com/UTDNebula" articleName="UTD Nebula"/></li>
-            <li><Article url="https://github.com/UTDNebula/survival-guide" articleName="Survival Guide"/></li>  
-        </ul>
-    )
-};
-
-function Article(props){
-    return(
-        <a href={props.url}>{props.articleName}</a>
     )
 };
