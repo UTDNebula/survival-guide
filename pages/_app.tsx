b999e1209b12d0d2ba56565fdc7e41804fd31089
Willie Chalmers III <willie@williecubed.me>
2023-02-16 19:45:36 -0600

    fix: Fix missing fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Roboto, Jost } from '@next/font/google';
+
+const sansFont = Roboto({
+  weight: '500',
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+const displayFont = Jost({
+  weight: '500',
+  subsets: ['latin'],
+  variable: '--font-display',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${sansFont.variable} ${displayFont.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
