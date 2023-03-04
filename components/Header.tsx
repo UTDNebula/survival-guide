35fc827aa4e934b407f238b52e275156f227926c
Willie Chalmers III <willie@williecubed.me>
2023-03-04 00:26:51 -0600

    chore: Update share button styles in content page

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,13 +12,19 @@ interface HeaderProps {
 export default function PageHeader({ title, url, date, contributors }: HeaderProps) {
   return (
     <div className="pt-8">
-      <div className="flex space-x-8">
-        <div className="text-[60px] font-bold font-display">{title}</div>
+      <div className="">
+        <div className="flex space-x-8">
+          <div className="text-[60px] font-bold font-display">{title}</div>
+        </div>
+        <div className="absolute top-4 right-4">
+          <ShareButton url={url} />
+        </div>
       </div>
-      <ShareButton url={url} />
-      <div className="font-display text-xl">Last Edited {date}</div>
-      <div className="font-display text-xl">Contributors</div>
-      <ContributorList contributors={contributors} />
+      <div className="mt-4">
+        <div className="font-display text-xl">Last Edited {date}</div>
+        <div className="font-display text-xl">Contributors</div>
+        <ContributorList contributors={contributors} />
+      </div>
     </div>
   );
 }
--- components/ShareButton.tsx
+++ components/ShareButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RWebShare } from 'react-web-share';
-import shareImg from '../public/Images/share.png';
+import ShareIcon from '../assets/Share.svg';
 
 interface ShareButtonProps {
   url: string;
@@ -15,9 +15,9 @@ export default function ShareButton({ url }: ShareButtonProps) {
         title: 'Survival Guide',
       }}
     >
-      <button className="inline-flex p-3 bg-[#EFEFEF] rounded-lg space-x-4 font-display text-lg hover:bg-[#E0E0E0] transition ease-linear">
-        <img className="" src={shareImg.src} alt="?" width="24" height="24" />
-        SHARE
+      <button className="inline-flex p-2 space-x-2 bg-[#EFEFEF] rounded-lg font-display text-lg hover:bg-[#E0E0E0] transition ease-linear">
+        <img className="" src={ShareIcon.src} alt="" width="24" height="24" />
+        Share
       </button>
     </RWebShare>
   );
