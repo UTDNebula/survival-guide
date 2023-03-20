16cf1472e354afb3dfddf5c1d0a9c603001d2e98
vikranthchinnam <vikranth.chinnam@gmail.com>
2023-03-20 11:37:45 -0500

    delte comments and change heading

--- pages/glossary.tsx
+++ pages/glossary.tsx
@@ -13,8 +13,8 @@ export default function glossary() {
       <main className="p-16">
         <div className="max-w-4xl mx-auto space-y-4">
           <div className="p-12 bg-white rounded-xl shadow-md">
-            <h1 className="text-headline4 text-[96px] font-bold font-display">I got you NIgga</h1>
-            <div className="text-4xl">We couldn&apos;t find what you were looking for NIgga.</div>
+            <h1 className="text-headline4 text-[96px] font-bold font-display">Work In progress</h1>
+            <div className="text-4xl">Will be filled in later</div>
             <div className="mt-4 text-lg text-2xl">
               Go{' '}
               <Link
@@ -67,33 +67,5 @@ export async function getStaticProps({
  * @returns Guide entry data
  */
 async function getEntryData(): Promise<GlossaryEntries> {
-    let list: string;
-    for(let i = 0; i < testGlossaryEntries.length; i++){
-        let entry : GlossaryEntry;
-        entry = {term: testGlossaryEntries[i].term, content: testGlossaryEntries[i].content}
-        list += {GlossaryEntryList : [entry]}
-    }
-    if(list === null){
-        throw new Error('Invalid ');
-    }
-    else{
-        return list;
-    }
-    
-    // const entry = testGlossaryEntries.find((entry) => list += [term: entry.term, content : entry.content]:Promise<GlossaryEntry>);
-    // if(list){
-    //     return list;
-    // }else{
-    //     throw new Error('Invalid entry ID provided');
-    // }
-    // if (entry) {
-    //   const data = {
-    //     ...entry,
-    //     lastUpdated: new Date(entry.lastUpdated).valueOf(),
-    //     content: (await remark().use(html).process(entry.content)).toString(),
-    //   };
-    //   return data;
-    // } else {
-    //   throw new Error('Invalid entry ID provided.');
-    // }
-  }
+    var list = testGlossaryEntries.map()
+}
