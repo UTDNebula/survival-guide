3c74bd0a5186c44159b9f4acd0d1472fb538c32a
vikranthchinnam <vikranth.chinnam@gmail.com>
2023-03-22 16:24:54 -0500

    Change Background, add footer, and adds the glossary entries to the page

--- pages/glossary.tsx
+++ pages/glossary.tsx
@@ -6,66 +6,68 @@ import type {
     GetStaticPropsResult,
     InferGetStaticPropsType,
   } from 'next/types';
+import SiteFooter from '../components/SiteFooter';
 
-export default function glossary() {
+export default function glossary(entry : InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(entry);
+  const glossaryEntries = entry.entry.map(glossaryEntry =>  
+    <div key={glossaryEntry.term}>
+      <p className='text-lg font-semibold'>{glossaryEntry.term}</p>
+      <br />
+      <p className='text-sm font-normal text-zinc-600'>{glossaryEntry.content}</p>
+      <br />
+    </div>   
+  );
   return (
-    <div className="min-h-screen bg-slate-100 bg-[url(/nebula_background.svg)] bg-cover">
-      <main className="p-16">
-        <div className="max-w-4xl mx-auto space-y-4">
-          <div className="p-12 bg-white rounded-xl shadow-md">
-            <h1 className="text-headline4 text-[96px] font-bold font-display">Work In progress</h1>
-            <div className="text-4xl">Will be filled in later</div>
-            <div className="mt-4 text-lg text-2xl">
-              Go{' '}
-              <Link
-                href="/"
-                className="text-primary font-semibold underline hover:text-primary-dark transition-all ease-in-out"
-              >
-                home
-              </Link>
-            </div>
-          </div>
+    <div className='bg-[#e0ecfb]'>
+    <main className="p-16">
+      <div className="max-w-4xl mx-auto space-y-4">
+        <div className="p-12 bg-white rounded-xl shadow-md">
+          <h1 className="text-headline4 text-[96px] font-bold font-display">Glossary</h1>
+          
+          <br />
+          {glossaryEntries}
+          
         </div>
-      </main>
+      </div>
+    </main>
+      <SiteFooter />
     </div>
   );
 }
 
 type GlossaryEntry = {
-    term: string,
-    content: string
+  term: string,
+  content: string
 };
 
-type GlossaryEntries = {
-    GlossaryEntryList: GlossaryEntry[];
-};
+type GlossaryEntries = GlossaryEntry[];
 
 type EntryPageProps = {
-    entry: GlossaryEntry;
+    entry: GlossaryEntries;
 };
 
 /**
- * Fetches entry data.
+ * Fetches glossary entry data.
  *
  * At build time, this gets the data for a Guide entry from our data source.
  */
 export async function getStaticProps({
     params,
   }: GetStaticPropsContext): Promise<GetStaticPropsResult<EntryPageProps>> {
-    const data = await getEntryData();
+    const data = await getGlossaryEntryData();
     return {
       props: {
-        entry: data,
-      },
+        entry: data
+      }
     };
 }
 
 /**
- * Fetches data for one Guide entry page.
+ * Fetches data of all glossary entries for the glossary page.
  *
- * @param id
- * @returns Guide entry data
+ * @returns  glossary entry data
  */
-async function getEntryData(): Promise<GlossaryEntries> {
-    var list = testGlossaryEntries.map()
+async function getGlossaryEntryData(): Promise<GlossaryEntries> {
+  return testGlossaryEntries;
 }
